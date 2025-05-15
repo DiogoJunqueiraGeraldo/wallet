7abce508c8e9a0f7cff29eb052d8cfbb1b019a97
Diogo Geraldo <diogojunqueirageraldo@gmail.com>
2025-05-15 10:42:23 -0300

    feat(command): add project from spring initializr

---
+++ wallets-command-service/src/main/java/com/wallet/wallets_command_service/WalletsCommandServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wallet.wallets_command_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletsCommandServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletsCommandServiceApplication.class, args);
+	}
+
+}
---
+++ wallets-command-service/src/test/java/com/wallet/wallets_command_service/WalletsCommandServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallet.wallets_command_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletsCommandServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
